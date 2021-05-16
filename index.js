2f8e9b4d81f461ae4ca4cfcef2194dddbd5c0665
Yan Moroz <yanchillosthegreat@yandex.ru>
2021-05-17 01:45:28 +0300

    feat: modal view with two text fields: name and about

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-let editModalWindow = document.querySelector('.edit-modal-window');
+let editPopup = document.querySelector('.popup');
 let editButton = document.querySelector('.profile__edit-button');
+let closePopupButton = document.querySelector('.popup__close-button');
+let nameLabel = document.querySelector('.profile__name');
+let aboutLabel = document.querySelector('.profile__about');
+let saveButton = document.querySelector('.popup__save-button');
 
+let nameTextField = document.querySelector('#name-input');
+let aboutTextField = document.querySelector('#about-input');
 
 editButton.addEventListener('click', function() {
-  editModalWindow.classList.add('edit-modal-window_opened');
+  nameTextField.value = nameLabel.textContent;
+  aboutTextField.value = aboutLabel.textContent;
+  editPopup.classList.toggle('popup_opened');
+});
+
+closePopupButton.addEventListener('click', function() {
+  editPopup.classList.toggle('popup_opened');
+});
+
+saveButton.addEventListener('click', function() {
+  nameLabel.textContent = nameTextField.value;
+  aboutLabel.textContent = aboutTextField.value;
+  editPopup.classList.toggle('popup_opened');
 });
